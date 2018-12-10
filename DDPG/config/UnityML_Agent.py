1546862b6237f2cee8627062d8de103b25d55b6b
DAVAAJAV Ganbold <ganbold_0324@yahoo.com>
2018-12-10 15:49:08 +0900

    fix: Change default hyperparameter

--- DDPG/config/UnityML_Agent.py
+++ DDPG/config/UnityML_Agent.py
@@ -6,22 +6,22 @@ HYPERPARAMS = {
         'env_name':             "Reacher",
         'stop_scores':          30.0,
         'scores_window_size':   100,
-        'train_episodes':       1000,
+        'train_episodes':       5,
 
         'replay_size':          1000000,            # replay buffer size
         'replay_initial':       1000,               # replay buffer initialize
         'update_interval':      1,
         'fix_target_updates':   1,                  # fix the target Q for the fix_target_updates
 
-        'actor_hidden_layers':  [256, 256],         # hidden units and layers of Actor-network
-        'critic_hidden_layers': [256, 256, 128],    # hidden units and layers of Critic-network
+        'actor_hidden_layers':  [64, 64],           # hidden units and layers of Actor-network
+        'critic_hidden_layers': [64, 64, 32],       # hidden units and layers of Critic-network
 
         'epsilon_start':        1.0,                # starting value of epsilon
         'epsilon_final':        0.05,               # minimum value of epsilon
         'epsilon_decay':        0.999,              # factor for decreasing epsilon
 
-        'actor_learning_rate':  5e-4,               # actor learning rate
-        'critic_learning_rate': 5e-4,               # critic learning rate
+        'actor_learning_rate':  1e-4,               # actor learning rate
+        'critic_learning_rate': 1e-4,               # critic learning rate
         'gamma':                0.99,               # discount factor
         'thau':                 1e-3,               # for soft update of target parameters
         'batch_size':           128,                # minibatch size
