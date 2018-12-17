1a59da901d1add42a55edad5798b331662c27fa8
DAVAAJAV Ganbold <ganbold_0324@yahoo.com>
2018-12-17 15:56:22 +0900

    test: Test under the best parameter config

--- MADDPG/config/UnityML_Agent.py
+++ MADDPG/config/UnityML_Agent.py
@@ -5,12 +5,12 @@ HYPERPARAMS = {
     'Tennis': {
         # Global parameters
         'random_seed':          0,                  # Random seed
-        'update_interval':      2,                  # Freeze the Agent for the update_interval
+        'update_interval':      1,                  # Freeze the Agent for the update_interval
 
         # Exploration parameters
         'noise_amplitude_start': 5.0,               # starting value of noise_amplitude
         'noise_amplitude_final': 0.1,               # minimum value of noise_amplitude
-        'noise_amplitude_decay': 0.994,             # factor for decreasing noise_amplitude
+        'noise_amplitude_decay': 0.9939,            # factor for decreasing noise_amplitude
 
         # Environment parameters
         'env_name':             "Tennis",
@@ -21,7 +21,7 @@ HYPERPARAMS = {
         # Replay buffer parameters
         'replay_size':          1000000,            # replay buffer size
         'replay_initial':       1000,               # replay buffer initialize
-        'batch_size':           256,                # minibatch size
+        'batch_size':           128,                # minibatch size
 
         # Actor parameters
         'actor_state_size':     0,
@@ -29,7 +29,7 @@ HYPERPARAMS = {
         'actor_hidden_layers':  [256, 128],         # hidden units and layers of Actor-network
         'actor_learning_rate':  1e-3,               # actor learning rate
         'actor_weight_decay':   0.0000,             # L2 weight decay
-        'actor_thau':           5e-2,               # for soft update of target parameters
+        'actor_thau':           0.043,              # for soft update of target parameters
 
         # Critic parameters
         'critic_state_size':    0,
@@ -37,7 +37,7 @@ HYPERPARAMS = {
         'critic_hidden_layers': [256, 128],         # hidden units and layers of Critic-network
         'critic_learning_rate': 1e-3,               # actor learning rate
         'critic_weight_decay':  0.0000,             # L2 weight decay
-        'critic_thau':          5e-2,               # for soft update of target parameters
+        'critic_thau':          0.043,              # for soft update of target parameters
 
         'gamma':                0.99,               # discount factor
     },
