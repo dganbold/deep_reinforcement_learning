0ed56c096fca2d4f1f9fa799114a483e14bb6513
DAVAAJAV Ganbold <ganbold_0324@yahoo.com>
2018-12-25 10:50:59 +0900

    feat: Add soccer config

--- MADDPG/config/UnityML_Agent.py
+++ MADDPG/config/UnityML_Agent.py
@@ -16,7 +16,7 @@ HYPERPARAMS = {
         'env_name':             "Tennis",
         'stop_scores':          0.5,
         'scores_window_size':   100,
-        'train_episodes':       1000,
+        'train_episodes':       800,
 
         # Replay buffer parameters
         'replay_size':          1000000,            # replay buffer size
@@ -27,7 +27,46 @@ HYPERPARAMS = {
         'actor_state_size':     0,
         'actor_action_size':    0,
         'actor_hidden_layers':  [256, 128],         # hidden units and layers of Actor-network
-        'actor_learning_rate':  1e-3,               # actor learning rate
+        'actor_learning_rate':  1e-4,               # actor learning rate
+        'actor_weight_decay':   0.0000,             # L2 weight decay
+        'actor_thau':           0.043,              # for soft update of target parameters
+
+        # Critic parameters
+        'critic_state_size':    0,
+        'critic_action_size':   0,
+        'critic_hidden_layers': [256, 128],         # hidden units and layers of Critic-network
+        'critic_learning_rate': 1e-3,               # actor learning rate
+        'critic_weight_decay':  0.0000,             # L2 weight decay
+        'critic_thau':          0.043,              # for soft update of target parameters
+
+        'gamma':                0.99,               # discount factor
+    },
+    'Soccer': {
+        # Global parameters
+        'random_seed':          0,                  # Random seed
+        'update_interval':      1,                  # Freeze the Agent for the update_interval
+
+        # Exploration parameters
+        'noise_amplitude_start': 5.0,               # starting value of noise_amplitude
+        'noise_amplitude_final': 0.1,               # minimum value of noise_amplitude
+        'noise_amplitude_decay': 0.9939,            # factor for decreasing noise_amplitude
+
+        # Environment parameters
+        'env_name':             "Soccer",
+        'stop_scores':          0.5,
+        'scores_window_size':   100,
+        'train_episodes':       800,
+
+        # Replay buffer parameters
+        'replay_size':          1000000,            # replay buffer size
+        'replay_initial':       1000,               # replay buffer initialize
+        'batch_size':           128,                # minibatch size
+
+        # Actor parameters
+        'actor_state_size':     0,
+        'actor_action_size':    0,
+        'actor_hidden_layers':  [256, 128],         # hidden units and layers of Actor-network
+        'actor_learning_rate':  1e-4,               # actor learning rate
         'actor_weight_decay':   0.0000,             # L2 weight decay
         'actor_thau':           0.043,              # for soft update of target parameters
 
